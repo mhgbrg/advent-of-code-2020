e5c89980da839a6c3fea1e636c06ddf1eb4ec7d6
Mats Högberg <mats@hgbrg.se>
2020-12-23 01:04:53 +0100

    Clean up solution for day 17

--- src/advent_of_code_2020/day_17.clj
+++ src/advent_of_code_2020/day_17.clj
@@ -2,24 +2,30 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as string]))
 
-;part 1
 (def input
   (->> "day-17.txt"
        (io/resource)
        (slurp)
        (string/split-lines)
        (mapv #(string/split % #""))
+       )
+  )
+
+;part 1
+(def input-3d
+  (->> input
        (map-indexed
          (fn [y row]
            (map-indexed (fn [x cell] [[x y 0] (case cell "#" :active nil)]) row)
            )
          )
        (apply concat)
+       (remove #(nil? (second %)))
        (into {})
        )
   )
 
-(defn adjacent-coords [[x y z]]
+(defn adjacent-coord-3d [[x y z]]
   (for [dx (range -1 2)
         dy (range -1 2)
         dz (range -1 2)
@@ -31,16 +37,16 @@
     )
   )
 
-(defn count-active-adjacent [state coord]
+(defn count-active-adjacent-3d [state coord]
   (->> coord
-       (adjacent-coords)
+       (adjacent-coord-3d)
        (map (partial get state))
        (remove nil?)
        (count))
   )
 
-(defn next-cube-state [state coord]
-  (let [num-active-adjacent (count-active-adjacent state coord)]
+(defn next-cube-state-3d [state coord]
+  (let [num-active-adjacent (count-active-adjacent-3d state coord)]
     (case (get state coord)
       :active (if (#{2 3} num-active-adjacent) :active nil)
       nil (if (= 3 num-active-adjacent) :active nil)
@@ -48,7 +54,7 @@
     )
   )
 
-(defn limits [state]
+(defn limits-3d [state]
   (let [[x-min y-min z-min] (->> state
                                  (keys)
                                  (apply map list)
@@ -65,41 +71,23 @@
     )
   )
 
-(defn next-state [state]
-  (->> (let [lims (limits state)]
+(defn next-state-3d [state]
+  (->> (let [lims (limits-3d state)]
          (for [x (apply range (:x lims))
                y (apply range (:y lims))
                z (apply range (:z lims))
                :let [coord [x y z]]]
-           [coord (next-cube-state state coord)]
+           [coord (next-cube-state-3d state coord)]
            )
          )
+       (remove #(nil? (second %)))
        (into {})
        )
   )
 
-(defn show-state [state z]
-  (let [lims (limits state)]
-    (string/join
-      "\n"
-      (for [y (apply range (:y lims))]
-        (string/join
-          ""
-          (for [x (apply range (:x lims))]
-            (case (get state [x y z])
-              :active "#"
-              nil "."
-              )
-            )
-          )
-        )
-      )
-    )
-  )
-
 (->> (reduce
-       (fn [state _cycle] (next-state state))
-       input
+       (fn [state _cycle] (next-state-3d state))
+       input-3d
        (range 6)
        )
      (vals)
@@ -108,23 +96,20 @@
      )
 
 ;part 2
-(def input-2
-  (->> "day-17.txt"
-       (io/resource)
-       (slurp)
-       (string/split-lines)
-       (mapv #(string/split % #""))
+(def input-4d
+  (->> input
        (map-indexed
          (fn [y row]
            (map-indexed (fn [x cell] [[x y 0 0] (case cell "#" :active nil)]) row)
            )
          )
        (apply concat)
+       (remove #(nil? (second %)))
        (into {})
        )
   )
 
-(defn adjacent-coords-2 [[x y z w]]
+(defn adjacent-coords-4d [[x y z w]]
   (for [dx (range -1 2)
         dy (range -1 2)
         dz (range -1 2)
@@ -138,34 +123,16 @@
     )
   )
 
-(defn limits-2 [state]
-  (let [[x-min y-min z-min w-min] (->> state
-                                 (keys)
-                                 (apply map list)
-                                 (map #(apply min %))
-                                 (map dec))
-        [x-max y-max z-max w-max] (->> state
-                                 (keys)
-                                 (apply map list)
-                                 (map #(apply max %))
-                                 (map #(+ 2 %)))]
-    {:x [x-min x-max]
-     :y [y-min y-max]
-     :z [z-min z-max]
-     :w [w-min w-max]}
-    )
-  )
-
-(defn count-active-adjacent-2 [state coord]
+(defn count-active-adjacent-4d [state coord]
   (->> coord
-       (adjacent-coords-2)
+       (adjacent-coords-4d)
        (map (partial get state))
        (remove nil?)
        (count))
   )
 
-(defn next-cube-state-2 [state coord]
-  (let [num-active-adjacent (count-active-adjacent-2 state coord)]
+(defn next-cube-state-4d [state coord]
+  (let [num-active-adjacent (count-active-adjacent-4d state coord)]
     (case (get state coord)
       :active (if (#{2 3} num-active-adjacent) :active nil)
       nil (if (= 3 num-active-adjacent) :active nil)
@@ -173,23 +140,42 @@
     )
   )
 
-(defn next-state-2 [state]
-  (->> (let [lims (limits-2 state)]
+(defn limits-4d [state]
+  (let [[x-min y-min z-min w-min] (->> state
+                                       (keys)
+                                       (apply map list)
+                                       (map #(apply min %))
+                                       (map dec))
+        [x-max y-max z-max w-max] (->> state
+                                       (keys)
+                                       (apply map list)
+                                       (map #(apply max %))
+                                       (map #(+ 2 %)))]
+    {:x [x-min x-max]
+     :y [y-min y-max]
+     :z [z-min z-max]
+     :w [w-min w-max]}
+    )
+  )
+
+(defn next-state-4d [state]
+  (->> (let [lims (limits-4d state)]
          (for [x (apply range (:x lims))
                y (apply range (:y lims))
                z (apply range (:z lims))
                w (apply range (:w lims))
                :let [coord [x y z w]]]
-           [coord (next-cube-state-2 state coord)]
+           [coord (next-cube-state-4d state coord)]
            )
          )
+       (remove #(nil? (second %)))
        (into {})
        )
   )
 
 (->> (reduce
-       (fn [state _cycle] (next-state-2 state))
-       input-2
+       (fn [state _cycle] (next-state-4d state))
+       input-4d
        (range 6)
        )
      (vals)
